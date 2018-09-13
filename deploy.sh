d9849946b8156e5592a517875b1ec7a07626aa6d
sigtot <sigurdtotland@gmail.com>
2018-09-14 01:35:18 +0200

    Check if go built successfully before copying binary to server (fix #3)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
 GOOS=linux GOARC=amd64 go build -o staging-server
-ssh vektorprogrammet@82.196.15.63 'sudo service staging-server stop'
-scp staging-server vektorprogrammet@82.196.15.63:/var/www/staging-server
-ssh vektorprogrammet@82.196.15.63 'sudo service staging-server start'
+if [ $? -eq 0 ]; then
+    ssh vektorprogrammet@82.196.15.63 'sudo service staging-server stop'
+    scp staging-server vektorprogrammet@82.196.15.63:/var/www/staging-server
+    ssh vektorprogrammet@82.196.15.63 'sudo service staging-server start'
+else
+    echo "Build was unsuccessful"
+fi
